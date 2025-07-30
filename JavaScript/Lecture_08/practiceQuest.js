9a8350e59ba090bf69a7881bd5b3d34b6d4b9832
Nandkishor-2103 <mandal.nandkishor@hotmail.com>
2025-07-30 19:49:41 +0530

    All Update

---
+++ JavaScript/Lecture_08/practiceQuest.js
@@ -0,0 +1,55 @@
+// Q1. 
+/* let arr = [1, 2, 3, 4, 5];
+
+let square = arr.map((ele) => ele * ele);
+console.log(square);
+
+let sum = arr.reduce((acc, el) => acc + el, 0);
+console.log(sum);
+
+console.log(sum/arr.length); */
+
+
+// Q2. 
+/* let arr = [1, 2, 3, 4, 5];
+
+let newArr = arr.map((el) => el + 5);
+
+console.log(newArr); */
+
+
+// Q3. 
+
+// let arr = ["radha", "balaram", "krishn", "madhav"];
+
+// let newArr = arr.map((el) => el.toUpperCase());
+// console.log(newArr);
+
+
+// Q4.
+
+/* const doubleAndReturnArgs = (arr, ...args) => [
+    arr, 
+    ...args.map((el) => el * 2)
+];
+
+
+console.log(doubleAndReturnArgs([1, 2, 3], 4, 5, 6));  */
+
+/* const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2});
+
+let obj1 = {
+    name: "radha",
+    id : 21
+}
+
+let obj2 = {
+    city : "Vrindavan",
+    dob: "21-12-2003"
+}
+
+// console.log(mergeObjects(obj1, obj2));
+let newObject = mergeObjects(obj1, obj2);
+console.log(newObject); */
+
+// console.log({a: 1, b: 2}, {c: 4, e: 5});
